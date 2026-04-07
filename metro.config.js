eb61dd4670c3e462e4427a59af84ba0d199fac08
Lucas Gonçalves Balduino <lucas.balduino@dinfor.uniceub>
2026-04-07 16:20:22 -0300

    Arquivo para interpretação de arquivos svg pelo react

---
+++ metro.config.js
@@ -0,0 +1,19 @@
+// Config file para tornar svg's legiveis para o reactive
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  return config;
+})();
